266ee9b95bc0ad77606b6cd30b88b3b63cfa319a
Xijun Zhu <zxj@vincross.com>
2019-11-11 19:09:30 +0800

    add message

--- services/service.go
+++ services/service.go
@@ -14,7 +14,9 @@ import (
 	// note: requires user to create k8s services
 	"github.com/micro/go-grpc"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/broker"
 	"github.com/micro/go-micro/server"
+	"github.com/micro/go-plugins/broker/kafka"
 	"vc.svc/models"
 	sequenceService "vc.svc/services/sequence"
 )
@@ -32,9 +34,14 @@ func Init(config models.MicroConfig) {
 }
 
 func (r *registrar) init(config models.MicroConfig) {
+
 	service := grpc.NewService(
 		micro.Name(config.Name),
 		micro.Version(config.Version),
+		micro.Broker(kafka.NewBroker(func(o *broker.Options) {
+			o.Addrs = []string{0: "127.0.0.1:9092"}
+			// o.Addrs = []string{0: "redis://localhost:6379/0"}
+		})),
 		micro.RegisterTTL(time.Second*30),
 		micro.RegisterInterval(time.Second*15),
 		micro.BeforeStart(r.beforeStart),
@@ -43,6 +50,17 @@ func (r *registrar) init(config models.MicroConfig) {
 		micro.AfterStop(r.afterStop),
 		micro.WrapHandler(r.logWrapper),
 	)
+
+	broker.Init()
+	broker.Connect()
+
+	broker.Subscribe("endpoint", func(p broker.Publication) error {
+		// brokerHeader := p.Message().Header
+		// aaa := brokerHeader["AAA"]
+		fmt.Printf("[%v] topic endpoint value: %s\n", time.Now(), string(p.Message().Body))
+		return nil
+	})
+
 	service.Init()
 	sequenceService.Register(service)
 
@@ -71,7 +89,11 @@ func (r *registrar) afterStop() error {
 
 func (r *registrar) logWrapper(fn server.HandlerFunc) server.HandlerFunc {
 	return func(ctx context.Context, req server.Request, rsp interface{}) error {
-		fmt.Printf("[%v] server request: %s\n", time.Now(), req.Endpoint())
+		// fmt.Printf("[%v] server request endpoint: %s header: %s body: %s \n", time.Now(), req.Endpoint(), req.Header(), req.Body())
+		broker.Publish("endpoint", &broker.Message{
+			Header: map[string]string{},
+			Body:   []byte(req.Endpoint()),
+		})
 		return fn(ctx, req, rsp)
 	}
 }
